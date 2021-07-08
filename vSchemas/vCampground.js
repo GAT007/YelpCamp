d02147a0a00a81ad7789703ec2bcfbccd89a30d2
Amith <amith.tallanki@gmail.com>
2021-07-08 22:16:25 +0530

    Deployment commit

--- vSchemas/vCampground.js
+++ vSchemas/vCampground.js
@@ -1,14 +1,38 @@
-const joi = require("joi");
+const baseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+    type : 'string',
+    base : joi.string(),
+    messages : {
+        'string.escapeHTML':'{{#label}} must not include HTML!'
+    },
+    rules : {
+        escapeHTML  : {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags : [],
+                    allowedAttributes : {},
+                });
+                if(clean!==value) return helpers.error('string.escapeHTML', {value})
+                return clean;
+            }
+        }
+    }
+})
+
+const joi = baseJoi.extend(extension);
+
 module.exports.vCampgroundSchema = joi.object({
-    title: joi.string().required(),
+    title: joi.string().required().escapeHTML(),
     price: joi.number().required().min(0),
-    description: joi.string().required(),
-    location: joi.string().required()
+    description: joi.string().required().escapeHTML(),
+    location: joi.string().required().escapeHTML()
 });
 
 module.exports.vReviewSchema = joi.object({
     review: joi.object({
         rating: joi.number().required().min(0).max(5),
-        body: joi.string().required(),
+        body: joi.string().required().escapeHTML(),
     }).required()
-})
+});
